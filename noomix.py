cc2a56359a31dfc981c83ef631d35a24756d8b3f
Alex Crist <accrist@gmail.com>
2016-02-05 03:12:06 -0700

    project complete

--- noomix.py
+++ noomix.py
@@ -1,12 +1,18 @@
 import random
 
-test_words = ["r", "revolt", "objection"]
+in_file = open('textinput.txt', mode='r')
 
-for test_word in test_words:
-    if len(test_word) == 1:
-        print(test_word)
-    else:
-        random_inner = list(test_word[1:-1])
-        random.shuffle(random_inner)
-        finished_word = test_word[0] + ''.join(random_inner) + test_word[-1]
-        print(finished_word)
+for line in in_file:
+    test_line = line.split()
+    for word in test_line:
+        test_word = word.strip('.:,-')
+        if len(test_word) == 1:
+            print(test_word, end=' ')
+        else:
+            random_inner = list(test_word[1:-1])
+            random.shuffle(random_inner)
+            finished_word = test_word[0] + ''.join(random_inner) + test_word[-1]
+            print(finished_word, end=' ')
+    print()
+
+in_file.close()
