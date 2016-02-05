6d012baf3fab88a0e48b45057ae8c36f5ca5c08c
Alex Crist <accrist@gmail.com>
2016-02-05 02:48:22 -0700

    basic functionality working. used list data structure because file objects are basically the same.

--- noomix.py
+++ noomix.py
@@ -1,4 +1,12 @@
-test_word = "revolt"
+import random
 
-for letter in test_word:
-    print(letter)
+test_words = ["r", "revolt", "objection"]
+
+for test_word in test_words:
+    if len(test_word) == 1:
+        print(test_word)
+    else:
+        random_inner = list(test_word[1:-1])
+        random.shuffle(random_inner)
+        finished_word = test_word[0] + ''.join(random_inner) + test_word[-1]
+        print(finished_word)
